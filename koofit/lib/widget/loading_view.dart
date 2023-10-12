547f9c9d049c1a25503d712542f3c5b02990bc4e
Hyeeun Shin <as221323@kookmin.ac.kr>
2023-10-12 17:37:11 +0900

    Create loading_view.dart

---
+++ koofit/lib/widget/loading_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:koofit/model/config/palette.dart';
+
+Widget loadingView() {
+  return WillPopScope(
+    onWillPop: () async {
+      return false;
+    },
+    child: Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Expanded(
+            child: Center(
+                child: CircularProgressIndicator(
+              color: Palette.mainSkyBlue,
+            )),
+          ),
+        ],
+      ),
+    ),
+  );
+}

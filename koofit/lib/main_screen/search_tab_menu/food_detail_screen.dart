1e1195ad672bd0bcaa37da69a69d5952183d2fd2
HyeEunShin <as221323@kookmin.ac.kr>
2023-11-27 16:48:00 +0900

    MODIFY  rowData -> food()

--- koofit/lib/main_screen/search_tab_menu/food_detail_screen.dart
+++ koofit/lib/main_screen/search_tab_menu/food_detail_screen.dart
@@ -1,92 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:koofit/model/config/palette.dart';
+import 'package:koofit/model/data/food.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   final List<String> rowData;
 
   DetailsPage({required this.rowData});
 
   @override
-  Widget build(BuildContext context) {
+  _DetailsPageState createState() => _DetailsPageState();
+}
 
-    print(rowData);
+class _DetailsPageState extends State<DetailsPage> {
+  Food? food;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (widget.rowData.length >= 21) {
+      food = Food(
+        foodCode: widget.rowData[0],
+        foodName: widget.rowData[1],
+        manufacturer: widget.rowData[2],
+        foodWeight: double.tryParse(widget.rowData[14]),
+        calories: double.tryParse(widget.rowData[16]),
+        protein: double.tryParse(widget.rowData[17]),
+        fat: double.tryParse(widget.rowData[18]),
+        carbo: double.tryParse(widget.rowData[19]),
+        sugar: double.tryParse(widget.rowData[20]),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(widget.rowData);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Palette.mainSkyBlue,
-        title: Text('영양성분',
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-      )),
-      body: ListView.builder(
-        itemCount: rowData.length,
-        itemBuilder: (context, index) {
-          if (rowData[index] ==''){
-            rowData[index] = '정보 없음';
-          }
-          switch (index) {
-            case 1:
-              return _buildCard('식품명 :', rowData[index]);
-              break;
-
-            case 2:
-              return _buildCard('제조사명 :', rowData[index]);
-              break;
-
-            case 14:
-              return _buildCard('식품 중량 :', rowData[index]);
-              break;
-
-            case 16:
-              return _buildCard('에너지(kcal) :', rowData[index]);
-              break;
-
-            case 17:
-              return _buildCard('단백질(g) :', rowData[index]);
-              break;
-
-            case 18:
-              return _buildCard('지방(g) :', rowData[index]);
-              break;
-
-            case 19:
-              return _buildCard('탄수화물(g) :', rowData[index]);
-              break;
-
-            case 20:
-              return _buildCard('당류(g) :', rowData[index]);
-              break;
-          }
-          return Container();
-        },
+        title: Text(
+          '영양성분',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              size: 29,
+            ),
+            onPressed: () {
+              print(widget.rowData);
+              // 아이콘 버튼이 눌렸을 때 수행할 동작 추가
+              print('Settings button pressed');
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 5),
+            _buildTile('식품명', '${food!.foodName }'),
+            _buildTile('식품 중량(g)', '${food?.foodWeight?? '정보없음'}'),
+            _buildTile('에너지(kcal)', '${food!.calories ?? '정보없음'}'),
+            _buildTile('탄수화물(g)', '${food?.carbo ?? '정보없음'}'),
+            _buildTile('당류(g)', '${food?.sugar ?? '정보없음'}'),
+            _buildTile('단백질(g)', '${food?.protein ?? '정보없음'}'),
+            _buildTile('지방(g)', '${food?.fat ?? '정보없음'}'),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildCard(String leadingText, String titleText) {
+  Widget _buildTile(String title, String value) {
     return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+        padding: EdgeInsets.symmetric(vertical: 4),
         child: Card(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      elevation: 2.0,
-      child: ListTile(
-        leading: Text(
-          leadingText,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-        ),
-        title: Text(titleText,
-          style: TextStyle(fontSize: 14),
-          textAlign: TextAlign.start,),
-      ),
-    ));
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            elevation: 2.0,
+            child: ListTile(
+                title: Row(children: [
+              Text('${title}  : ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+              Text(value, style: TextStyle(fontSize: 15))
+            ]))));
   }
 }
-
-
-
-// if (index == 0 || index == 3 ) {
-// return ListTile(
-// leading: Text(
-// 'index $index',
-// ),
-// title: Text(rowData[index]),
-// );
-// }

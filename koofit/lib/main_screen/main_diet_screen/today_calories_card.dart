46348ec80fe1b0858aea320547ebcde0514af07c
HyeEunShin <as221323@kookmin.ac.kr>
2023-11-29 00:10:25 +0900

    MODIFY 총섭취칼로리 표시

--- koofit/lib/main_screen/main_diet_screen/today_calories_card.dart
+++ koofit/lib/main_screen/main_diet_screen/today_calories_card.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:koofit/main_screen/main_diet_screen/today_show.dart';
 import 'package:koofit/main_screen/search_tab_menu/search_diet_screen.dart';
+import 'package:koofit/model/HiveDietHelper.dart';
 import 'package:koofit/model/config/palette.dart';
 import 'package:get/get.dart';
+import 'package:koofit/model/data/diet.dart';
 import 'package:koofit/widget/circleText.dart';
+import 'package:koofit/widget/loading_view.dart';
 
 class TodayCalorieCard extends StatefulWidget {
   @override
@@ -12,15 +15,54 @@ class TodayCalorieCard extends StatefulWidget {
 
 class _TodayCalorieCardState extends State<TodayCalorieCard> {
   bool isOuter = true;
+  bool isSuccess = false;
+  late String todayDate;
+  late List<Diet> dietList;
+
+  double totalCalories = 0.0;
+  Map<String, dynamic> todayDietMap = {
+    "totalCalo": "",
+    "carboRate": "",
+    "proteinRate": "",
+    "fatRate": ""
+  };
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    todayDate = DateTime.now().toLocal().toString().split(' ')[0];
+print(todayDate);
+    HiveDietHelper().readDiet();
+
+    HiveDietHelper().searchDiet(todayDate).then((value) {
+      dietList = value;
+    print(dietList);
+     totalCalories  = (dietList.fold<double>(0.0, (sum, diet) => sum + (diet.nutrient.calories ?? 0.0)));
+
+     setState(() {
+       isSuccess = true;
+     });
+
+    });
+
+
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return
+    Container(
         decoration: BoxDecoration(
           borderRadius:
               BorderRadius.circular(35), // 여기서 숫자를 조절하여 둥근 정도를 결정할 수 있습니다.
         ),
-        child: InkWell(
+        child:
+        isSuccess ?
+        InkWell(
             onTap: () async {
               await showTodayDiet(context);
             },
@@ -45,7 +87,7 @@ class _TodayCalorieCardState extends State<TodayCalorieCard> {
                             height: 5,
                           ),
                           Text(
-                            "0 Kal",
+                            "${totalCalories}Kal",
                             style: TextStyle(
                               color: Color(0xFF222B45),
                               fontSize: 18,
@@ -58,6 +100,11 @@ class _TodayCalorieCardState extends State<TodayCalorieCard> {
                           CircleText(Palette.tanSu, 61, isOuter),
                           CircleText(Palette.danBaek, 100, isOuter),
                           CircleText(Palette.jiBang, 24, isOuter)
-                        ])))));
+                        ]))))
+    : Center(
+            child: CircularProgressIndicator(
+              color: Palette.mainSkyBlue,
+            ))
+    );
   }
 }

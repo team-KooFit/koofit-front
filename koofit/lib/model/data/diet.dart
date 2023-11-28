e2cb0e2746b9d6cc32a9d804d657fa714ee33813
HyeEunShin <as221323@kookmin.ac.kr>
2023-11-29 00:09:30 +0900

    MODIFY diet() format 변경

--- koofit/lib/model/data/diet.dart
+++ koofit/lib/model/data/diet.dart
@@ -4,57 +4,53 @@ part 'diet.g.dart';
 
 @HiveType(typeId: 3)
 class Diet {
-  @HiveField(0)
-  String uid;
 
-  @HiveField(1)
+  @HiveField(0)
   String stuNumber;
 
-  @HiveField(2)
+  @HiveField(1)
   String date; //등록한 날짜
 
-  @HiveField(3)
+  @HiveField(2)
   String keyTime; //등록한 시간대 (아침/점심/저녁/간식)
 
-  @HiveField(4)
-  String foodCode;
+  @HiveField(3)
+  String foodName;
 
-  @HiveField(5)
+  @HiveField(4)
   Food nutrient; //영양성분 :[{'포카칩' : foodData, {'주스' : {...} } ] 아니면 { 이름 : '포카칩', 식품코드: '100000' }
 
   Diet(
-      {required this.uid,
+      {
       required this.stuNumber,
       required this.date,
       required this.keyTime,
-      required this.foodCode,
+      required this.foodName,
       required this.nutrient});
 
   factory Diet.fromJson(Map<String, dynamic> json) {
     return Diet(
-      uid: json['uid'],
       stuNumber: json['stu_number'],
       date: json['date'],
       keyTime: json['keyTime'],
-      foodCode: json['foodCode'],
+      foodName: json['foodName'],
       nutrient: Food.fromJson(Map<String, dynamic>.from(json['nutrient'])),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'uid': uid,
       'stuNumber': stuNumber,
       'date': date,
       'keyTime': keyTime,
-      'foodCode': foodCode,
+      'foodCode': foodName,
       'nutrient': nutrient
     };
   }
 
   @override
   String toString() {
-    return '\nDiet{uid: $uid, stuNumber: $stuNumber, date: $date, keyTime: $keyTime, foodCode: $foodCode, nutrient: $nutrient}';
+    return '\nDiet{ stuNumber: $stuNumber, date: $date, keyTime: $keyTime, foodName: $foodName, nutrient: $nutrient}';
   }
 
 }

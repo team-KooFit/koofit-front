f0a3dd983e6a0af5deaf3efc97419954c2129094
HyeEunShin <as221323@kookmin.ac.kr>
2023-12-01 19:28:36 +0900

    MODIFY 다른날짜, 운동기록 에러 해결

--- koofit/lib/main_screen/fitness_screen/fitness_card.dart
+++ koofit/lib/main_screen/fitness_screen/fitness_card.dart
@@ -21,9 +21,18 @@ class FitnessCard extends StatefulWidget {
 
 class _FitnessCardState extends State<FitnessCard> {
   bool isOuter = true;
+  bool isSuccess = false;
 
-  late List<Fitness> todayFitness;
+  late List<Fitness> todayFitness = [];
   int totalTime = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _initializeData(widget.selectedDate);
+  }
 
   @override
   void didUpdateWidget(covariant FitnessCard oldWidget) {
@@ -34,21 +43,18 @@ class _FitnessCardState extends State<FitnessCard> {
   }
 
   Future<void> _initializeData(String date) async {
-    HiveFitnessHelper().readFitness();
-
     print("110112903108230192301 $date");
-    HiveFitnessHelper().searchFitness(date).then((value) {
-      todayFitness = value;
-      print("111110011001011010101010 $todayFitness");
 
-      // 총 Time data 추출
-      totalTime = todayFitness.fold<int>(
-        0,
-        (sum, fitness) => sum + (fitness.time ?? 0),
-      );
+    todayFitness = await HiveFitnessHelper().searchFitness(date);
+    totalTime = todayFitness.fold<int>(
+      0,
+      (sum, fitness) => sum + (fitness.time ?? 0),
+    );
 
-      print(totalTime);
+    setState(() {
+      isSuccess = true;
     });
+    print("${todayFitness} ///\n ${widget.selectedDate}, ${totalTime}");
   }
 
   @override
@@ -59,42 +65,50 @@ class _FitnessCardState extends State<FitnessCard> {
           borderRadius:
               BorderRadius.circular(35), // 여기서 숫자를 조절하여 둥근 정도를 결정할 수 있습니다.
         ),
-        child: InkWell(
-            onTap: () async {
-              await ShowFitness(context, widget.selectedDate);
-            },
-            child: Card(
-                color: Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 5),
-                          Text(
-                            "운동",
-                            style: TextStyle(
-                              color: Color(0xA5222B45),
-                              fontSize: 15,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "$totalTime분/60분",
-                            style: TextStyle(
-                              color: Color(0xFF222B45),
-                              fontSize: 18,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 13,
-                          ),
-                        ])))));
+        child: isSuccess
+            ? InkWell(
+                onTap: () async {
+                  await ShowFitness(context, widget.selectedDate);
+                },
+                child: Card(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 5),
+                              Text(
+                                "운동",
+                                style: TextStyle(
+                                  color: Color(0xA5222B45),
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                "${totalTime ?? 0}분/60분",
+                                // 나머지 코드...
+
+                                style: TextStyle(
+                                  color: Color(0xFF222B45),
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 13,
+                              ),
+                            ]))))
+            : Center(
+                child: CircularProgressIndicator(
+                color: Palette.mainSkyBlue,
+              )));
   }
 }
